800d77990fb5299376362bdeb98c8e59683bb68b
user name <user email>
2021-06-16 12:30:59 +0530

    Ability TO Get Name as Argument IN Curl Commands

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -1,5 +1,7 @@
 package com.example.demo.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,6 +17,11 @@ public class HelloController {
 	@RequestMapping(method = RequestMethod.GET, value = {"/query"})
 	public String sayHello(@RequestParam(value = "name") String name) {
 		return "Hello "+name+"!";
+		
+	}
+	@GetMapping("/param/{name}")
+	public String sayHelloParam(@PathVariable String name) {
+		return "Hello "+name+ "!";
 		
 	}
 	
