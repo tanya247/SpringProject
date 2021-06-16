652ee54cf457e253a8437f9229bb3f852c889b62
user name <user email>
2021-06-16 22:49:43 +0530

    Put Call IN Curl Commands

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -3,6 +3,7 @@ package com.example.demo.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -37,7 +38,10 @@ public class HelloController {
 	}
 	
 	
-	
+	@PutMapping("/put/{firstName}")
+	public String sayHelloPut(@PathVariable String firstName, @RequestParam(value= "lastName") String lastName) {
+		return "Hello" + firstName + " " + lastName + "!";
+	}
 	
 
 }
