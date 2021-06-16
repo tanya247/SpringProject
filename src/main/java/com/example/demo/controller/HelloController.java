bb3156f0f61e00b30f023fd63b4b7a9ba7618c1f
user name <user email>
2021-06-16 12:18:55 +0530

    Ability TO Get Hello By Query IN Curl Commands

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -1,6 +1,8 @@
 package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,6 +11,11 @@ public class HelloController {
 	@RequestMapping(value = {"","/","/home"})
 	public String sayHello() {
 		return "Hello";
+	}
+	@RequestMapping(method = RequestMethod.GET, value = {"/query"})
+	public String sayHello(@RequestParam(value = "name") String name) {
+		return "Hello "+name+"!";
+		
 	}
 	
 	
